bafbbe9779804a694214fc97acadfbed55855d4c
Lucasclase <lucasperezparracho@gmail.com>
2023-11-22 13:03:15 +0100

    Plugin_v1

---
+++ src/wp-content/plugins/censored.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package Palabra Censurada
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Palabra Censurada
+Plugin URI: http://wordpress.org/plugins/Palabra-Censurada/
+Description: Este plugin censura las palabras malsonantes en WordPress.
+Author: Lucas_dam1
+Version: 1.0.0
+Author URI: http://ma.tt/
+*/
+
+# list of offensive words
+$offensiveWordsList = [
+    'puta',
+    'puto',
+    'putas',
+    'putos',
+    'cabron',
+    'cabrona',
+    'cabrones',
+    'cabronas',
+    'gilipollas'
+
+];
+$nonOffensiveWordsList = [
+    'promiscua',
+    'promiscuo',
+    'promiscuas',
+    'promiscuos',
+    'cabrito',
+    'cabrita',
+    'cabritos',
+    'cabritas',
+    'gilipichis'
+
+];
+function renym_wordpress_typo_fix( $text ) {
+    global $offensiveWordsList, $nonOffensiveWordsList;
+    return str_replace( $offensiveWordsList, $nonOffensiveWordsList, $text );
+}
+
+add_filter('the_content', 'renym_wordpress_typo_fix');
